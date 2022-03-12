e161b358273e86ed70c1ad8c65c8307f57a704d0
George Gomes <georgevag@protonmail.com>
2022-03-11 21:19:33 -0300

    Add avarage and bhaskara exercises

---
+++ EstruturaDeDados/codes/Aula2/Avarage.js
@@ -0,0 +1,6 @@
+let nota1 = 7;
+let nota2 = 10;
+
+let media = (nota1 + nota2)/2;
+console.log("A média é ", media);
+
---
+++ EstruturaDeDados/codes/Aula2/Bhaskara.js
@@ -0,0 +1,38 @@
+
+// Bhaskara correta
+var a = 1;
+var b = 12;
+var c = -13;
+
+// Delta negativo
+// var a = 2;
+// var b = 2;
+// var c = 2;
+
+// Delta igual a 0
+// var a = 1;
+// var b = -10;
+// var c = 25;
+
+
+// Delta = b² - 4.a.c
+const delta =  (b*b) - 4 * a * c
+console.log("Delta: ", delta);
+
+// x = – b ± √Δ
+//        2·a
+
+if (delta < 0) { 
+    console.log("O delta é negativo. Equação não possui raízes reais.")
+} else if (a === 0) { 
+    console.log("O A não pode ser igual a 0")
+} else { 
+    const raizDelta = Math.sqrt(delta);
+    const x1 = ((-b) + raizDelta)/(2*a);
+    const x2 = ((-b) - raizDelta)/(2*a);
+    console.log(" as raízes da equação são ", x1 ," e ", x2);
+}
+
+
+
+
