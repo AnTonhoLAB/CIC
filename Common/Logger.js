d9403fe86a979d162bd4bf4215f60c22063c7b10
George Gomes <georgevag@protonmail.com>
2022-03-16 20:22:06 -0300

    Fix logger and add first code in class 3

--- Common/Logger.js
+++ Common/Logger.js
@@ -6,15 +6,15 @@ function startApp() {
     if (isLogable) { 
         console.clear();
         t0 = performance.now();
-        console.log('------ Programa Iniciado ------');
+        console.log('------ Programa Iniciado ------ \n');
     }
 }
 
 function finishApp() { 
     if (isLogable) { 
         const t1 = performance.now();
-        console.log(`----- Programa Finalizado -----`);
-        console.log(`tempo de execução: ${t1 - t0} milliseconds.`);
+        console.log(`\n----- Programa Finalizado -----`);
+        console.log(`tempo de execução: ${t1 - t0} milliseconds. \n`);
     }
 }
 
--- EstruturaDeDados/codes/Aula3/Class3.js
+++ EstruturaDeDados/codes/Aula3/Class3.js
@@ -1,12 +1,22 @@
 // Class 3 16/03/2022
 
 const logger = require('../../../Common/Logger');
+const prompt = require('../../../Common/PromptSync/prompt');
 
 logger.startApp();
 
-for (i = 0; i<10; i++) { 
-    console.log(i)
+const notaA1 = 3
+const notaA2 = 1
+const notaFinal = notaA1 + notaA2
+
+if (notaFinal >= 6) { 
+    console.log("Aprovado");
+} else if (notaFinal < 6 && notaFinal >=5) { 
+    console.log("Recuperação");
+} else { 
+    console.log("Reprovado");
 }
 
-logger.finishApp();
+console.log("Com a nota", notaFinal);
 
+logger.finishApp();
