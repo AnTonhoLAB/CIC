7302ce4849a4dbc7c494aa53510145b03fe180b9
George Gomes <georgevag@protonmail.com>
2022-03-24 20:25:40 -0300

    Setup exercise1

--- EstruturaDeDados/codes/Aula4/Exercise1.js
+++ EstruturaDeDados/codes/Aula4/Exercise1.js
@@ -1,11 +1,15 @@
 const logger = require('../../../Common/Logger');
 const prompt = require('../../../Common/PromptSync/prompt');
 
+const description = "1 - Escreva um algoritmo para ler um valor (do teclado) e escrever (na tela) o seu antecessor" 
+
 function presentsExercise() { 
     const number = prompt("Digite um numero: ", "number");
     const result = previous(number);
+    logger.separation();
     const message = `O número anterior a ${number} é ${result}`
-    logger.logl(message)
+    logger.log(message)
+    logger.separation();
 }
 
 function previous(number) { 
@@ -13,5 +17,6 @@ function previous(number) {
 }
 
 module.exports = {
+    description,
     presentsExercise
 }
