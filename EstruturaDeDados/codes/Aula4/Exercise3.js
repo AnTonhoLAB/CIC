149aab70b0c52718d14e8b440b932a3d25e0f70b
George Gomes <georgevag@protonmail.com>
2022-03-24 21:24:39 -0300

    Finish Exercise3

--- EstruturaDeDados/codes/Aula4/Exercise3.js
+++ EstruturaDeDados/codes/Aula4/Exercise3.js
@@ -4,7 +4,25 @@ const prompt = require('../../../Common/PromptSync/prompt');
 const description = '3 - Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e \n    escreva a idade dessa pessoa expressa apenas em dias. Considerar ano com 365 dias e mês \n    com 30 dias.'
 
 function presentsExercise() { 
-  
+    const years = prompt('Digite os anos: ', 'number');
+    const months = prompt('Digite os meses: ', 'number');
+    const days = prompt('Digite os dias: ', 'number');
+
+    logger.separation();
+    const result = calcuateDaysBy(years, months, days);
+
+    const pluralYears = years == 1 ? 'ano' : 'anos';
+    const pluralMonths = months == 1 ? 'mes' : 'meses';
+    const pluralDays = days == 1 ? 'dia' : 'dias';
+    const resultPlural = result == 1 ? 'dia' : 'dias';
+
+    const message = `${years} ${pluralYears} ${months} ${pluralMonths} e ${days} ${pluralDays} \né igual a ${result} ${resultPlural}`
+    logger.log(message)
+    logger.separation();
+}
+
+function calcuateDaysBy(years, months, days) { 
+    return ((years * 360) + (months * 30) + days)
 }
 
 module.exports = {
