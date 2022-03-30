3bae70bb669d003b488206f83131ad6c31f4d013
George Gomes <georgevag@protonmail.com>
2022-03-30 19:14:21 -0300

    Finish ex5

--- EstruturaDeDados/codes/Aula4/Exercise5.js
+++ EstruturaDeDados/codes/Aula4/Exercise5.js
@@ -4,7 +4,15 @@ const prompt = require('../../../Common/PromptSync/prompt');
 const description = '5 - Escreva um algoritmo para ler o salário mensal atual de um funcionário e o percentual de \n    reajuste. Calcular e escrever o valor do novo salário'
 
 function presentsExercise() { 
-  
+    const salary = prompt('Digite o valor do salário mensal atual: ', 'number');
+    const percentage = prompt('Digite o valor do reajuste em %: ', 'number');
+
+    const reajust = encrease(salary, percentage);
+    logger.separationIn(`Com um reajuste de ${percentage}% \no salario vai de: R$ ${salary} \npara: R$ ${reajust}`);
+}
+
+function encrease(salary, percentage) { 
+    return (((salary * percentage) / 100) + salary)
 }
 
 module.exports = {
