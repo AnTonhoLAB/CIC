b79a5d5a3898a4d3db6ffea346b1a0d5f6ecc46e
George Gomes <georgevag@protonmail.com>
2022-03-31 23:04:43 -0300

    Fix ex7 statement

--- EstruturaDeDados/codes/Aula4/Exercise7.js
+++ EstruturaDeDados/codes/Aula4/Exercise7.js
@@ -1,7 +1,7 @@
 const logger = require('../../../Common/Logger');
 const prompt = require('../../../Common/PromptSync/prompt');
 
-const description = '7 - Uma revendedora de carros usados paga a seus funcionários vendedores um salário fixo \n    por mês, mais uma comissão também fixa para cada carro vendido e mais 5% do valor das \n vendas por ele efetuadas. Escrever um algoritmo que leia o número de carros por ele \n    vendidos, o valor total de suas vendas, o salário fixo e o valor que ele recebe por carro \n    vendido. Calcule e escreva o salário final do vendedor'
+const description = '7 - Uma revendedora de carros usados paga a seus funcionários vendedores um salário fixo \n    por mês, mais uma comissão também fixa para cada carro vendido e mais 5% do valor das \n    vendas por ele efetuadas. Escrever um algoritmo que leia o número de carros por ele \n    vendidos, o valor total de suas vendas, o salário fixo e o valor que ele recebe por carro \n    vendido. Calcule e escreva o salário final do vendedor'
 
 function presentsExercise() { 
   
