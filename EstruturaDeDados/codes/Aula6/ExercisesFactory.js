e583c74baa90b895666d9cadd5a33457f0284391
George Gomes <georgevag@protonmail.com>
2022-04-12 23:00:39 -0300

    Add ex2 on factory

--- EstruturaDeDados/codes/Aula6/ExercisesFactory.js
+++ EstruturaDeDados/codes/Aula6/ExercisesFactory.js
@@ -1,5 +1,5 @@
 const exercise1 = require('../Aula6/Exercise1');
-// const exercise2 = require('../Aula6/Exercise2');
+const exercise2 = require('../Aula6/Exercise2');
 // const exercise3 = require('../Aula6/Exercise3');
 // const exercise4 = require('../Aula6/Exercise4');
 // const exercise5 = require('../Aula6/Exercise5');
@@ -9,8 +9,8 @@ const exercise1 = require('../Aula6/Exercise1');
 // const exercise9 = require('../Aula6/Exercise9');
 
 module.exports = {
-    exercises: [exercise1]//,
-    // exercise2,
+    exercises: [exercise1,
+    exercise2//,
     // exercise3,
     // exercise4,
     // exercise5,
@@ -18,5 +18,5 @@ module.exports = {
     // exercise7,
     // exercise8,
     // exercise9
-    // ]
+    ]
 }
