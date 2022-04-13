26862c16c76c59f4f83ea621a633245b284d5856
George Gomes <georgevag@protonmail.com>
2022-04-12 23:00:52 -0300

    Add ex 2 file with description

---
+++ EstruturaDeDados/codes/Aula6/Exercise2.js
@@ -0,0 +1,22 @@
+const logger = require('../../../Common/Logger');
+const prompt = require('../../../Common/PromptSync/prompt');
+const bubble = require('../Aula5/Class5');
+
+const description = "Faça um programa que leia n nomes inserindo-os em uma lista de form\nordenada utilizando a ideia do algoritmo Insertionsort. No final, o programa \ndeve mostrar todos os nomes ordenados alfabeticamente." 
+
+function presentsExercise() { 
+    // logger.separation();
+    // var text = prompt("Digite um texto: ");
+    // var arrayText = Array.from(text)
+
+    // logger.log(arrayText);
+    // bubble.bubbleSortFor(arrayText);
+    // logger.log(arrayText);
+
+    // logger.separation();
+}
+
+module.exports = {
+    description,
+    presentsExercise
+}
