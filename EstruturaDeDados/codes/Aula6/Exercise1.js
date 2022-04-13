efad7128793e4a7434695b48759f51a6e90529be
George Gomes <georgevag@protonmail.com>
2022-04-12 22:48:50 -0300

    Read directly from string

--- EstruturaDeDados/codes/Aula6/Exercise1.js
+++ EstruturaDeDados/codes/Aula6/Exercise1.js
@@ -6,13 +6,13 @@ const description = "1 - Crie um programa que dado uma string, coloque as letras
 
 function presentsExercise() { 
     logger.separation();
-    var text = prompt("Digite um texto: ", "arrayofstr");
-    var arrayText = Array.from(text.reduce((pre,curr)=>pre+curr, ""))
+    var text = prompt("Digite um texto: ");
+    var arrayText = Array.from(text)
 
     logger.log(arrayText);
     bubble.bubbleSortFor(arrayText);
     logger.log(arrayText);
-    
+
     logger.separation();
 }
 
