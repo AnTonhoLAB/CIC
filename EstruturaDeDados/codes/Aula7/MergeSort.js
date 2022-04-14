874320fa2c5e4f17bb4b3d799b33a3fe16264d96
George Gomes <georgevag@protonmail.com>
2022-04-13 22:23:56 -0300

    Add merge sort

---
+++ EstruturaDeDados/codes/Aula7/MergeSort.js
@@ -0,0 +1,51 @@
+
+
+function mergeSort(x, start, end) { 
+    let middle 
+    if (start < end) { 
+        middle = Math.trunc((start + end) / 2)
+        console.log(middle)
+        mergeSort(x, start, middle)
+        mergeSort(x, middle + 1, end)
+        interpolate(x, start, end, middle) 
+    } 
+}
+
+function interpolate(x, start, end, middle) { 
+    let freePos, vetStart1, vetStart2
+    let aux = []
+    vetStart1 = start
+    vetStart2 = middle + 1
+    freePos = start
+    
+    while (vetStart1 <= middle && vetStart2 <= end) { 
+        if(x[vetStart1] <= x[vetStart2]) {
+            aux[freePos] = x[vetStart1]
+            vetStart1 = vetStart1 + 1
+        } else { 
+            aux[freePos] = x[vetStart2]
+            vetStart2 = vetStart2 + 1
+        }
+        freePos = freePos + 1
+    }
+
+    for (let i = vetStart1; i <= middle; i++) { 
+        aux[freePos] = x[i]
+        freePos = freePos + 1
+    }
+
+    for (let i = vetStart2; i <= end; i++) { 
+        aux[freePos] = x[i]
+        freePos = freePos + 1
+    }
+
+    for (let i = vetStart1; i <= end; i++) { 
+        x[i] = aux[i]
+    }
+}
+let arrayToSort = [5,4,6,7,1,8,2,9]
+
+console.log(arrayToSort)
+mergeSort(arrayToSort, 0, arrayToSort.length -1)
+console.log(arrayToSort)
+
