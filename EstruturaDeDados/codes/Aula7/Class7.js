f0335401505201559fc2e710b290376f5e296df5
George Gomes <georgevag@protonmail.com>
2022-04-13 21:23:59 -0300

    Add Class7.js

---
+++ EstruturaDeDados/codes/Aula7/Class7.js
@@ -0,0 +1,28 @@
+let text = "Oi seja bem vindo"
+
+
+// Com casting
+let arrayOfCharactersA = Array.from(text)
+console.log(arrayOfCharactersA)
+
+// Com map
+let arrayOfCharactersB = [...text].map((x) => x)
+console.log(arrayOfCharactersB)
+
+// Com for
+let arrayOfCharactersC = []
+for (let charactere of text) {
+    arrayOfCharactersC.push(charactere)
+}
+
+console.log(arrayOfCharactersC)
+
+var dict = {
+    Name: "Eric",
+    Age: 23,
+    Job: "Freelancer",
+    Skills : "JavaScript"
+  };
+
+  console.log(dict['Job'])
+  console.log(dict.Job)
